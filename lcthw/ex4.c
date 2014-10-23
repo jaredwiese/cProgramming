07e05b6d96a136b38b5f20b2dbcd5f3941fb599a
Jared <wiesejc@users.noreply.github.com>
2014-10-23 04:01:41 -0500

    Create ex4.c
    http://valgrind.org/

---
+++ lcthw/ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Warning: This program is wrong on purpose.
+Using this to learn Valgrind */
+
+int main()
+{
+    int age = 10;
+    int height;
+
+    printf("I am %d years old.\n");
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}
