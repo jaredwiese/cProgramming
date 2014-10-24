8ed3672c67ed2f57e894fb4532acb426f26265b3
Jared <wiesejc@users.noreply.github.com>
2014-10-24 02:51:34 -0500

    Update ex16.c

--- lcthw/ex16.c
+++ lcthw/ex16.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Introducing structures; in C is similar to a row of a database table or a class in an OOP language
+
 struct Person {
     char *name;
     int age;
@@ -10,11 +12,13 @@ struct Person {
     int weight;
 };
 
+// struct Person is now a compound data type
 struct Person *Person_create(char *name, int age, int height, int weight)
 {
-    struct Person *who = malloc(sizeof(struct Person));
+    struct Person *who = malloc(sizeof(struct Person)); //memory allocate to ask the OS to give us a pice of raw memory
+    // ensure we have a valid piece of memory back from malloc
     assert(who != NULL);
-
+    // initialize each field, use strdup to duplicate the string for the name
     who->name = strdup(name);
     who->age = age;
     who->height = height;
@@ -23,14 +27,16 @@ struct Person *Person_create(char *name, int age, int height, int weight)
     return who;
 }
 
+// If we have a create, then we always need a destroy function
 void Person_destroy(struct Person *who)
 {
     assert(who != NULL);
-
+    // return the memory we got with malloc and strdup
     free(who->name);
     free(who);
 }
 
+// prints out the people
 void Person_print(struct Person *who)
 {
     printf("Name: %s\n", who->name);
@@ -39,16 +45,17 @@ void Person_print(struct Person *who)
     printf("\tWeight: %d\n", who->weight);
 }
 
+// create four people
 int main(int argc, char *argv[])
 {
-    // make two people structures
+    // make four people structures
     struct Person *ironman = Person_create(
             "Ironman", 44, 74, 220);
 
     struct Person *hulk = Person_create(
             "The Hulk", 44, 69, 128);
-
-	struct Person *cptamerica = Person_create(
+            
+    struct Person *cptamerica = Person_create(
             "Captain America", 96, 74, 240);
 
     struct Person *thor = Person_create(
@@ -60,8 +67,8 @@ int main(int argc, char *argv[])
 
     printf("The Hulk is at memory location %p:\n", hulk);
     Person_print(hulk);
-
-	printf("Captain America is at memory location %p:\n", cptamerica);
+    
+    printf("Captain America is at memory location %p:\n", cptamerica);
     Person_print(cptamerica);
 
     printf("Thor is at memory location %p:\n", thor);
@@ -76,10 +83,10 @@ int main(int argc, char *argv[])
     hulk->weight += 1000;
     Person_print(hulk);
 
-    // destroy them all so we clean up
+    // destroy them all so we clean up; if we don't do this we get a 'memory leak'
     Person_destroy(ironman);
     Person_destroy(hulk);
-	Person_destroy(cptamerica);
+    Person_destroy(cptamerica);
     Person_destroy(thor);
 
     return 0;
